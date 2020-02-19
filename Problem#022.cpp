c01fac458a1abab183c167e96eb30a4b320ef7a7
Amit Kumar <offamitkumar@gmail.com>
2020-02-19 23:27:02 +0530

    Problem 022 added

--- Problem#022.cpp
+++ Problem#022.cpp
@@ -1,3 +1,34 @@
+/*
+ *
+ *  This problem was asked by Microsoft.
+
+    Given a dictionary of words and a string made up of those words (no spaces), 
+    return the original sentence in a list. 
+    If there is more than one possible reconstruction, return any of them. 
+    If there is no possible reconstruction, then return null.
+
+    For example, given the set of words 'quick', 'brown', 'the', 'fox', and 
+    the string "thequickbrownfox", you should return ['the', 'quick', 'brown', 'fox'].
+
+    Given the set of words 'bed', 'bath', 'bedbath', 'and', 'beyond', and 
+    the string "bedbathandbeyond", return either ['bed', 'bath', 'and', 'beyond] or ['bedbath', 'and', 'beyond'].
+ *
+ *
+ *
+ */
+
+
+
+/* 
+ *
+ * Idea is use Trie and store the words in our tree and then in the 
+ * given string for each character we search in the tree if not found then 
+ * answer can't be constructed , we will handle it by making the okay [bool variable ] false
+ *
+ * otherwise we will get the answer in the  end and print that ;
+ *
+ *
+ */
 #include <bits/stdc++.h>
 #define ALPHABETS 26
 using namespace std;
