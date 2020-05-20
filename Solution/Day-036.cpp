d6616bee945cbae2cdf47ab567af33c0e3bcbe42
Amit Kumar <offamitkumar@gmail.com>
2020-05-20 13:28:56 +0530

    find 2nd largest Node

---
+++ Solution/Day-036.cpp
@@ -0,0 +1,46 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+    int ans =-1;
+    bool isAnsFound = false;
+    int tempCounter = 0;
+    void getans(TreeNode* root , int k){
+        if(isAnsFound){
+            return;
+        }
+        if(root->right!=nullptr){
+            getans(root->right , k);
+        }
+        tempCounter++;
+        if(k==tempCounter){
+            isAnsFound = true;
+            ans = root -> val;
+            tempCounter++;
+            return;
+        }
+        if(!isAnsFound && root->left != nullptr){
+            getans(root->left , k);
+        }
+        if(k==tempCounter){
+            isAnsFound = true;
+            ans = root -> val;
+            return;
+        }
+    }
+public:
+    int kthSmallest(TreeNode* root) {
+        tempCounter =0;
+        isAnsFound = false;
+        getans(root , 2);
+        return ans;
+    }
+};
