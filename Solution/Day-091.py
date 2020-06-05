385c922670c119fd7875d6dfb8e289ae945df359
Amit Kumar <offamitkumar@gmail.com>
2020-06-05 08:50:54 +0530

    correct the function - Dropbox

--- Solution/Day-091.py
+++ Solution/Day-091.py
@@ -1,7 +1,9 @@
+# it will print 9 , as first for loop exit at 9 
+# so 'i' will have value 9 at the end of for loop & all lambda function use 
+# i as their return value
 functions = []
 for i in range(10):
     functions.append(lambda : i)
-# print(i)
 i = 0
 for f in functions:
     print(f())
