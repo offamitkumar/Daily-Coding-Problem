02a70aa0741ada561dbed9579f29995dbff464fa
Amit Kumar <offamitkumar@gmail.com>
2020-06-07 10:34:14 +0530

    check validity of binary search tree - LinkedIn

---
+++ Solution/Day-089.cpp
@@ -0,0 +1,24 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool isValidBST(TreeNode* root, const std::optional<int64_t> &min_value=LLONG_MIN , const std::optional<int64_t>&max_value=LLONG_MAX) {
+        if(root == nullptr){
+            return true;
+        }
+        if(root -> val >= max_value.value() || root->val <= min_value.value()){
+            return false;
+        }
+        return isValidBST(root->right,root->val,max_value) && isValidBST(root->left,min_value,root->val);
+    }
+};
+
