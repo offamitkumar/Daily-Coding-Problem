02d89056976b9ce8f78229ff0b4311baf84cde75
Amit Kumar <offamitkumar@gmail.com>
2021-09-06 11:21:13 +0530

    neural language processing

---
+++ Solution/Day-351.cpp
@@ -0,0 +1,8 @@
+This seems a Neural Language Processing problem.
+
+The general idea to solve this problem is, to create vector for each of the word , let's just say 
+our attributes are : name , height , nature , color , age 
+then these attributes have different value for a person , country , material. So by supervised or un-supervised 
+learning we can prepare the model and predict the output. 
+
+solving this problem is somewhat creating a whole new project, so let's just skip this one. 
