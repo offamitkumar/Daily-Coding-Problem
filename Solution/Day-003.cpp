6ab3a574dba789308129bf94004f7360b1b56153
offamitkumar <offamitkumar@gmail.com>
2021-10-18 11:41:05 +0530

    binary tree

--- Solution/Day-003.cpp
+++ Solution/Day-003.cpp
@@ -1,87 +1,65 @@
-/*
- * This problem was asked by Google.  
- *
- * Given the root to a binary tree, implement serialize(root), which serializes the tree into a string, 
- * and deserialize(s), which deserializes the string back into the tree.  
- * For example, given the following Node class
- *
- * class Node:
-    def __init__(self, val, left=None, right=None):
-        self.val = val
-        self.left = left
-        self.right = right
- *
- *
- * The following test should be passed
- *
- * node = Node('root', Node('left', Node('left.left')), Node('right'))
- * assert deserialize(serialize(node)).left.left.val == 'left.left'
- *
- *
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
  */
+class Codec {
+    queue<string>q;
+    TreeNode* desr(void) {
+        if (q.empty()) {
+            return nullptr;
+        }
+        string temp = q.front(); 
+        q.pop(); 
+        if (temp =="&") {
+            return nullptr; 
+        }
+        TreeNode* root = new TreeNode(atoi(temp.c_str())); 
+        root -> left = desr(); 
+        root -> right = desr(); 
+        return root; 
+    }
+    string s; 
+    void h_ser(TreeNode* root) {
+        if (root == nullptr) {
+            s +=  "&/";
+            return ;
+        }
+        s += to_string(root->val) + "/"; 
+        h_ser(root->left); 
+        h_ser(root->right);
+    }
+public:
 
+    // Encodes a tree to a single string.
+    string serialize(TreeNode* root) {
+        s = ""; 
+        h_ser(root);
+        return s;
+    }
 
-#include <bits/stdc++.h>
-using namespace std;
-class Node {
-    public:
-        string value;
-        Node *left = nullptr;
-        Node *right = nullptr;
+    // Decodes your encoded data to tree.
+    TreeNode* deserialize(string data, int index = 0) {
+        string temp{}; 
+        for (int i=0; i<data.size(); ++i) {
+            if (data[i] == '/') {
+                q.push(temp); 
+                temp ="";
+                continue; 
+            }
+            temp += data[i]; 
+        }
+        return desr(); 
+    }
 };
-string serialize(Node *root){
-    if(root==nullptr){
-        return "&"; // separator & represent that current Node is empty 
-    }
-    return root->value+"/"+serialize(root->left)+"/"+serialize(root->right); // here / represent that data of current is completely processed
-}
 
-Node * de_serialize(string &s, int &index){
-    string temp = "";
-    for(;(index<(int)s.size()) && (s[index]!='&' && s[index]!='/');++index){
-        temp+=(s[index]);
-    }
-    if(temp=="&" || temp==""){
-        return nullptr;
-    }
-    Node *t = new Node();
-    t->value= temp;
-    index+=1;
-    t->left = de_serialize(s , index);
-    index+=1;
-    t->right = de_serialize(s,index);
-    return t;
-}
-
-void pre_order(Node *R){
-    if(R==nullptr){
-        return ;
-    }
-    cout<<R->value<<endl;
-    pre_order(R->left);
-    pre_order(R->right);
-}
-int main(void){
-    Node *root = new Node();
-    root->value = "root";
-    Node *l1 = new Node();
-    l1->value = "left";
-    Node *ll1 = new Node();
-    ll1->value = "left.left";
-    Node *r1 = new Node();
-    r1->value = "right";
-
-    root->left = l1;
-    root->right = r1;
-    l1->left = ll1;
-
-    string s = serialize(root);
-
-    // de-serializing the string into binary tree 
-    
-    int index = 0;
-    Node *R = de_serialize(s,index);
-    pre_order(R); // pre-order on de-serialized tree 
-    assert(R->left->left->value=="left.left"); // condition from google 
-    return 0;
-}
+// Your Codec object will be instantiated and called as such:
+// Codec* ser = new Codec();
+// Codec* deser = new Codec();
+// string tree = ser->serialize(root);
+// TreeNode* ans = deser->deserialize(tree);
+// return ans;
